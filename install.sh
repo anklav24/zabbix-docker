8f4fa4d3ed514241b2cf1e3860ddd8b0fa3c0462
Aleksandr Lobanov <anklav24@gmail.com>
2021-10-27 00:20:29 +0700

    Changed: install.sh

--- install.sh
+++ install.sh
@@ -2,13 +2,18 @@
 
 sudo apt update
 
-# Install programs (Optional)
+echo
+echo "------Install programs (Optional)------"
+echo
 
-sudo apt -y mc ncdu
+sudo apt install -y mc ncdu
 
-# Install Docker
+echo
+echo "------Install docker------"
+echo
+
 sudo apt -y install apt-transport-https ca-certificates curl gnupg lsb-release
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --batch --yes --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) \
@@ -18,12 +23,18 @@ echo \
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER   # Чтобы каждый раз не вводить sudo перед docker (Заработает после перезагрузки)
 
-# Install Docker Compose
+echo
+echo "------Install docker-compose------"
+echo
+
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version  # Test the installation.
 
-# Install ctop
+echo
+echo "------Install ctop------"
+echo
+
 echo "deb http://packages.azlux.fr/debian/ buster main" | sudo tee /etc/apt/sources.list.d/azlux.list
 wget -qO - https://azlux.fr/repo.gpg.key | sudo apt-key add -
 sudo apt update
@@ -32,6 +43,9 @@ sudo apt -y install docker-ctop
 # Install a stack
 sudo mkdir -p ~/zabbix-docker/zbx_env/usr/share/zabbix/modules
 cd ~/zabbix-docker
-docker-compose up -d
 
-sudo reboot now  # У меня дальше без перезагрузки не сработало
+#docker-compose up -d
+#docker-compose -f zabbix-server-docker-compose.yaml up -d
+#docker-compose -f zabbix-web-docker-compose.yaml up -d
+
+sudo reboot now  # Перезагружаем и смотрим что все стартовало
