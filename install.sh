f1ae73880b0f830d79a2c43b50211e021a50b53a
Aleksandr Lobanov <anklav24@gmail.com>
2021-10-27 00:34:11 +0700

    Fixed: install.sh docker install bug.

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+echo
+echo "------Update packages------"
+echo
+
 sudo apt update
 
 echo
@@ -19,6 +23,8 @@ echo \
   "deb [arch=$(dpkg --print-architecture) \
   signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt update
 
 sudo apt -y install docker-ce docker-ce-cli containerd.io
 sudo usermod -aG docker $USER   # Чтобы каждый раз не вводить sudo перед docker (Заработает после перезагрузки)
